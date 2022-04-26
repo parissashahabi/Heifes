00bde9f503b876793c44e0c40e477d919fa80983
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2022-04-27 00:24:03 +0430

    edit success receipt page

--- page-components/receipt/components/success.tsx
+++ page-components/receipt/components/success.tsx
@@ -1,20 +1,24 @@
 import { Button, Col, Row, Typography } from "antd";
 import success from "./success.module.scss";
+import {CheckCircleFilled} from "@ant-design/icons";
+
+const { Text, Title } = Typography;
 const Success = () => {
   return (
     <Col className={success["container"]} span={24}>
       <Col className={success["first-col"]}>
+        <Row><CheckCircleFilled style={{ fontSize: '80px', color: '#2D8F56' }} /></Row>
         <Row>
-          <Typography>پرداخت شما با موفقیت انجام شد</Typography>
+          <Title level={4} style={{color: '#2D8F56'}}>پرداخت شما با موفقیت انجام شد</Title>
         </Row>
         <Row>
-          <Typography>کد پیگیری: {}</Typography>
+          <Title level={5} style={{color: '#707070'}}>کد پیگیری: {}</Title>
         </Row>
       </Col>
       
-        <Button >دریافت رسید</Button>
+        <Button className={success["btn-receipt"]} >دریافت رسید</Button>
       
-        <Button>بازگشت به فروشگاه</Button>
+        <Button className={success["btn-return"]}>بازگشت به فروشگاه</Button>
      
     </Col>
   );
