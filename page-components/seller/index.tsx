9d8161dca1b078a738570858cce24768cd6a72b3
Mohammad <mohmmadweb@gmail.com>
2022-04-28 17:57:29 +0430

    base of layout

--- page-components/seller/index.tsx
+++ page-components/seller/index.tsx
@@ -2,10 +2,38 @@ import { Layout, Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import React, { Component } from 'react';
 import styles from "./index.module.scss";
+import ColumnGroup from 'antd/lib/table/ColumnGroup';
+import Column from 'antd/lib/table/Column';
 
 
 
 const { Header, Content, Footer, Sider } = Layout;
+const data = [
+  {
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
 export default () => (
   <Layout>
@@ -38,7 +66,38 @@ export default () => (
       <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
       <Content style={{ margin: '24px 16px 0' }}>
         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-          content
+        <Table dataSource={data}>
+    <ColumnGroup title="Name">
+      <Column title="First Name" dataIndex="firstName" key="firstName" />
+      <Column title="Last Name" dataIndex="lastName" key="lastName" />
+    </ColumnGroup>
+    <Column title="Age" dataIndex="age" key="age" />
+    <Column title="Address" dataIndex="address" key="address" />
+    <Column
+      title="Tags"
+      dataIndex="tags"
+      key="tags"
+      render={tags => (
+        <>
+          {tags.map(tag => (
+            <Tag color="blue" key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </>
+      )}
+    />
+    <Column
+      title="Action"
+      key="action"
+      render={(text, record) => (
+        <Space size="middle">
+          <a>Invite {record.lastName}</a>
+          <a>Delete</a>
+        </Space>
+      )}
+    />
+  </Table>
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Mohammad Aghaei</Footer>
