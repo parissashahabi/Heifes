fb32fa1b095d9d7f89922fdb4102de829fbe0177
parisa <prisashahbi@gmail.com>
2022-05-17 10:55:25 +0430

    test configuration

---
+++ tests/login.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { useRouter } from "next/router";
+
+jest.mock("next/router");
+
+describe("Authenticate route", ()=>{
+    it("Should return 405 if the method is not POST",()=>{
+
+    })
+})
