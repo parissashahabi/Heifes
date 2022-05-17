770af10f9b1f54ac39d9390fe2a02dec1c0a98da
parisa <prisashahbi@gmail.com>
2022-05-17 23:11:47 +0430

    fixed a bug

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,5 +1,5 @@
 import nc from 'next-connect';
-import Product from '../../../models/Product';
+import Product from '../../../models/product';
 import db from '../../../utils/db';
 
 const handler = nc();
