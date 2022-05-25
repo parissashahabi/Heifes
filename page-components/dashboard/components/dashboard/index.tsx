cea05ef2e7ba7a113f91568b782ddd955d7e2da2
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2022-05-25 14:26:08 +0430

    edit profile in dashboard tab for customer

--- page-components/dashboard/components/dashboard/index.tsx
+++ page-components/dashboard/components/dashboard/index.tsx
@@ -6,32 +6,40 @@ import changePassword from "../../../../common/functions/change-password";
 import CustomerAvatar from "../../../../public/icons/customerAvatar.svg";
 
 
+
 const DashboardPage = () => {
-    const [formRef] = Form.useForm();
     const { state, dispatch } = useContext(Store);
     const { userInfo } = state;
-    const onFinish = (values) => {
-        const query = {
-            userInfo: userInfo,
-            dispatch: dispatch,
-            oldPassword: values.oldPassword,
-            newPassword: values.newPassword,
-            confirmNewPassword: values.confirmNewPassword,
-            type: "customers",
-            formRef: formRef
-        };
-        changePassword(query);
-    };
+
     return (
         <Row justify={"space-between"}>
-            <Col className={styles["profile"]}>
-                <Row justify={"center"} style={{margin: "20px", width:"100px"}}>
-                    <CustomerAvatar />
+            <Col  className={styles["profile"]}>
+                <Row  justify={"center"} >
+                    <CustomerAvatar style={{margin: "20px", width:"100px"}} />
+                    <Row justify={"center"}>
+                        <Typography.Text
+                            style={{color:"#7E7E7E", fontSize:"16px" ,fontWeight:"500" }}>
+                            name
+                        </Typography.Text>
+                        <Typography.Text
+                            style={{color:"#A0A0A0", fontSize:"16px" ,fontWeight:"500" }}>
+                            phoneNumber
+                        </Typography.Text>
+                        <Button  className={styles["edit-btn"]}>
+                            ویرایش پروفایل
+                        </Button>
+                    </Row >
+
                 </Row>
+
 
             </Col>
             <Col className={styles["order-list"]}>
-                <Typography.Title> سفارشات جاری</Typography.Title>
+                <Typography.Title
+                    level={3} style={{color:"#707070", margin:"20px" }}>
+                    سفارشات جاری
+                </Typography.Title>
+
             </Col>
         </Row>
     );
