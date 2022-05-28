d9a573bde9e7d0022a5ee9e9f6479a77f3d9a28c
parisa <prisashahbi@gmail.com>
2022-05-28 07:31:44 +0430

    setup sentry nd fixed a bug in shopping cart

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,11 +1,59 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { useRouter } from "next/router";
+import assert from "assert";
+import { createServer } from "http";
+import nc from 'next-connect';
 
-jest.mock("next/router");
+import handler from "../pages/api/customers/login";
 
-describe("Authenticate route", ()=>{
-    it("Should return 405 if the method is not POST",()=>{
+describe("POST: /api/customer/login", () => {
+    it("Should returns an object with `isAdmin` property", () => {
+        const req = {
+            body: JSON.stringify({
+                phoneNumber: "09367205062", password: "111111"
+            })
+        };
+        const json = jest.fn();
+        const status = jest.fn(() => {
+            return {
+                json
+            }
+        })
+        const res = {
+            status
+        }
 
-    })
-})
+       handler(req, res);
+        console.log(json.mock);
+        console.log(json.mock.calls);
+    });
+    // let req;
+    // let res;
+    //
+    // beforeEach(() => {
+    //     req = {
+    //         method: "POST",
+    //         body: { phoneNumber: "abc@abc.com", password: "abc123" },
+    //     };
+    //
+    //     res = {
+    //         status: jest.fn(() => res),
+    //         end: jest.fn(),
+    //     };
+    // });
+    // it("Should returns an object with `isAdmin` property", async () => {
+    //     const app = createServer(handler);
+    //     return request(app)
+    //         .post("/api/customer/login",{ phoneNumber: "09367205062", password: "111111" })
+    //         .expect((response) => {
+    //             expect(response.body).toBeInstanceOf(Object);
+    //             expect(response.body).toHaveProperty("_id");
+    //         });
+    // });
+    //
+    // // it("The message should be equals to `hello jon-snow`", async () => {
+    // //     await testClient(handler)
+    // //         .get("/jon-snow")
+    // //         .expect((response) => {
+    // //             expect(response.body.message).toBe("hello jon-snow");
+    // //         });
+    // // });
+});
