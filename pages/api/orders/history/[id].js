dbe86df9b739c593313ecf5b9b82933d23089a48
parisa <prisashahbi@gmail.com>
2022-06-03 14:46:50 +0430

    test stocks api

--- pages/api/orders/history/[id].js
+++ pages/api/orders/history/[id].js
@@ -3,6 +3,7 @@ import Order from '../../../../models/order';
 import { isAuth } from '../../../../utils/auth';
 import db from '../../../../utils/db';
 import { onError } from '../../../../utils/error';
+import Customer from "../../../../models/customer";
 
 const handler = nc({
     onError,
@@ -11,7 +12,7 @@ handler.use(isAuth);
 
 handler.get(async (req, res) => {
     await db.connect();
-    const orders = await Order.find({ supermarketId: req.query.id }).populate('customer', ['name']);
+    const orders = await Order.find({ supermarketId: req.query.id }).populate('customer', 'name', Customer);
     res.send(orders);
 });
 
